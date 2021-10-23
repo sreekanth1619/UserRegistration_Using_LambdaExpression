d1cff91e42eb4445f6af53a30654e7915812b1df
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-23 14:38:57 +0530

    Added: As a User need to enter a valid First Name

---
+++ userregistration/src/main/java/com/bridgelabs/userregistration/UserRegistration.java
@@ -0,0 +1,20 @@
+package com.bridgelabs.userregistration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Purpose  - As a User need to enter a valid First Name
+ * @author  - N Sreekanth
+ * @version - 16.0
+ * @since   - 2021-10-02
+ */
+
+public class UserRegistration {
+
+	public boolean firstName(String firstName) {
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
+}
