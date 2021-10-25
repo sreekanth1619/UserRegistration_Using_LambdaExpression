7f9de98b9de70450e59b9d9c9613fc0a47b95ca5
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-25 11:38:47 +0530

    Added: To validate all the use case through exception handling

---
+++ userregistration/src/main/java/com/bridgelabs/userregistration/MoodAnalyserException.java
@@ -0,0 +1,17 @@
+package com.bridgelabs.userregistration;
+
+public class MoodAnalyserException extends Exception {
+
+    public String message;
+    public static ExceptionType type;
+
+    public enum ExceptionType {
+        EMPTY, NULL;
+    }
+
+    public MoodAnalyserException(ExceptionType type, String message) {
+        this.message = message;
+        this.type = type;
+    }
+
+}
